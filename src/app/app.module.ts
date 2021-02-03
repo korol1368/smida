db4a8fca8e21ba6e01f5ad907d673b150af89199
Aleksandr Korol <korol1368@gmail.com>
2021-02-03 20:38:46 +0200

    create component history

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HistoryComponent } from './history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
