b6ebc836bcea98df708e2d4baed530ab6d3bca31
Aleksandr Korol <korol1368@gmail.com>
2021-02-04 17:04:07 +0200

    template history table + table with data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {MatTableModule} from '@angular/material/table';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HistoryComponent} from './history/history.component';
 import {HistoryService} from "./services/history.service";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -15,7 +18,10 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [HistoryService],
   bootstrap: [AppComponent]
--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,16 +1,50 @@
-import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Component, AfterViewInit} from '@angular/core';
+import {merge, of as observableOf} from 'rxjs';
+import {catchError, map, startWith, switchMap} from 'rxjs/operators';
+import {HistoryModel} from "../models/history.model";
+import {HistoryService} from "../services/history.service";
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.scss']
 })
-export class HistoryComponent implements OnInit {
+export class HistoryComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'publicationType',
+    'termType',
+    'reportGroup',
+    'reportState',
+    'reportFormat',
+    'outputDate',
+    'outputNumber'
+  ];
+  data: HistoryModel[] = [];
 
-  constructor() {
+  resultsLength = 0;
+  isLoadingResults = true;
+
+  constructor(private http: HttpClient, private historyService: HistoryService) {}
+
+  ngAfterViewInit() {
+    merge()
+      .pipe(
+        startWith({}),
+        switchMap(() => {
+          this.isLoadingResults = true;
+          return this.historyService.getList();
+        }),
+        map(data => {
+          this.isLoadingResults = false;
+          this.resultsLength = data.length;
+
+          return data;
+        }),
+        catchError(() => {
+          this.isLoadingResults = false;
+          return observableOf([]);
+        })
+      ).subscribe(data => this.data = data);
   }
-
-  ngOnInit(): void {
-  }
-
 }
