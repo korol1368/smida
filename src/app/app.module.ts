eadc53b7c8d0c119be42f8f7e252cfd8451a9c6d
Aleksandr Korol <korol1368@gmail.com>
2021-02-04 15:34:05 +0200

    add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HistoryComponent} from './history/history.component';
 import {HistoryService} from "./services/history.service";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import {HistoryService} from "./services/history.service";
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [HistoryService],
   bootstrap: [AppComponent]
